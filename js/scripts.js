03927aae7477ff65eb7657b9481c2df1cb04b2f6
irina-pen <78215754+irina-pen@users.noreply.github.com>
2021-03-06 12:56:17 +0100

    add testimonials and card for home page

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+console.log("Hey! Welcome to my Portfolio Site")
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+x.className += ' responsive';
+}
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myNavtoggle").style.top = "0";
+  } else {
+    document.getElementById("myNavtoggle").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+/*testimonials*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
