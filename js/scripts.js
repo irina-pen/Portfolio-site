beb638940f4bfca274fbdba68be7e1fbe4c9dd0f
irina-pen <78215754+irina-pen@users.noreply.github.com>
2021-03-08 09:58:08 +0100

    fix issues with header on about and index pages

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,72 @@ function menuToggle() {
   }
 }
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+
+(function(){
+
+  var doc = document.documentElement;
+  var w = window;
+
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
+
+  var header = document.getElementById('header');
+
+  var checkScroll = function() {
+
+    /*
+    ** Find the direction of scroll
+    ** 0 - initial, 1 - up, 2 - down
+    */
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) {
+      //scrolled up
+      direction = 2;
+    }
+    else if (curScroll < prevScroll) {
+      //scrolled down
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+
+    prevScroll = curScroll;
+  };
+
+  var toggleHeader = function(direction, curScroll) {
+    if (direction === 2 && curScroll > 99) {
+
+      //replace 82 with the height of your header in px
+
+      header.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      header.classList.remove('hide');
+      prevDirection = direction;
+    }
+  };
+
+  window.addEventListener('scroll', checkScroll);
+
+})();
+
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 500) {
+            $(".header").addClass("header-active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("header-active");
+        }
+    });
+});
 
 /*testimonials*/
 var slideIndex = 1;
